1b113bcd0fb54707aad968592689f6ddd8f0098a
Woo Jin Park <woojinp@gmail.com>
2013-05-30 23:31:43 -0700

    implemented anchor scrolling

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,6 +1,17 @@
-// $(document).ready(function() {
-//   $('.summary').columnize({ width: 200 });
-// });
-
+$(document).ready(function() {
+  var $root = $('html, body');
+  $('a').click(function() {
+    var href = $.attr(this, 'href');
+    $root.animate({
+        scrollTop: $(href).offset().top - 75
+    }, 500, function () {
+        window.location.hash = href;
+    });
+    return false;
+  });
+  // $('#email_box').waypoint('sticky', {
+  //   offset: 30 // Apply "stuck" when element 30px from top
+  // });
+});
 
 
