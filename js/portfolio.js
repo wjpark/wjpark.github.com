2783b72b51466dc5d2952a34ac9759d1e8a29577
Woo Jin Park <woojinp@gmail.com>
2013-06-01 16:11:35 -0700

    pushing latest to work remotely

--- js/portfolio.js
+++ js/portfolio.js
@@ -4,11 +4,12 @@ $(document).ready(function() {
     var href = $.attr(this, 'href');
     $root.animate({
         scrollTop: $(href).offset().top - 75
-    }, 500, function () {
+    }, 2000,"easeInOutExpo", function () {
         window.location.hash = href;
     });
     return false;
   });
+
   // $('#email_box').waypoint('sticky', {
   //   offset: 30 // Apply "stuck" when element 30px from top
   // });
